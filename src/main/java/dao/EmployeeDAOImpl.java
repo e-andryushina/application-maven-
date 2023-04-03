7bd48a3c9978178e33005024de799c484b281697
e-andryushina <elena-and@inbox.ru>
2023-04-03 20:01:25 +0300

    Homework_application_maven_changes

--- src/main/java/dao/EmployeeDAOImpl.java
+++ src/main/java/dao/EmployeeDAOImpl.java
@@ -11,8 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
-    Connection connection;
-    Employee employee;
+    private final Connection connection;
+
 
     public EmployeeDAOImpl(Connection connection) {
         this.connection = connection;
@@ -20,14 +20,15 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public void create(Employee employee) {
+
         try(PreparedStatement statement = connection.prepareStatement(
                 "INSERT INTO employee(first_Name, last_Name, gender, age, city_id) VALUES ((?), (?), (?), (?), (?))")) {
             statement.setString(1, employee.getFirstName());
             statement.setString(2, employee.getLastName());
             statement.setString(3, employee.getGender());
             statement.setInt(4, employee.getAge());
-            statement.setInt(5, employee.getCity().getId());
-            statement.executeQuery();
+            statement.setInt(5, employee.getCity());
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -35,17 +36,18 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public Employee getById(int id) {
+        Employee employee = new Employee();
         try(PreparedStatement statement = connection.prepareStatement(
                 "SELECT * FROM employee INNER JOIN city ON employee.city_id=city.city_id AND id=(?)")) {
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                employee.setId(Integer.parseInt(resultSet.getString("id")));
+                employee.setId(resultSet.getInt("id"));
                 employee.setFirstName(resultSet.getString("first_name"));
                 employee.setLastName(resultSet.getString("last_name"));
                 employee.setGender(resultSet.getString("gender"));
-                employee.setAge(Integer.parseInt(resultSet.getString("age")));
-                employee.setCity(new City(resultSet.getInt("city_id"),resultSet.getString("city_name")));
+                employee.setAge(resultSet.getInt("age"));
+                employee.setCity(resultSet.getInt("city_name"));
             }
         } catch (SQLException e) {
            e.printStackTrace();
@@ -59,13 +61,13 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         try(PreparedStatement statement = connection.prepareStatement(
                 "SELECT * FROM employee INNER JOIN city ON employee.city_id=city.city_id")) {
             ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                int id = Integer.parseInt(resultSet.getString("id"));
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
                 String firstName = resultSet.getString("first_name");
                 String lastName = resultSet.getString("last_name");
                 String gender = resultSet.getString("gender");
-                int age = Integer.parseInt(resultSet.getString("age"));
-                City city = new City(resultSet.getInt("city_id"),resultSet.getString("city_name"));
+                int age = resultSet.getInt("age");
+                int city = resultSet.getInt("city_name");
 
                 employees.add(new Employee(id, firstName, lastName, gender, age, city));
             }
@@ -92,7 +94,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         try(PreparedStatement statement = connection.prepareStatement(
                 "DELETE FROM employee WHERE id=(?)")) {
             statement.setInt(1, id);
-            statement.executeQuery();
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
